316270e3a7f580994429b4b94ba7990ec378b306
FelipeIgansi <lipeigansi@gmail.com>
2023-08-01 14:56:51 -0300

    Make a dialog in a conversation

--- app/src/main/java/com/intuitivetools/primeirotestecomkotlindsl/MainActivity.kt
+++ app/src/main/java/com/intuitivetools/primeirotestecomkotlindsl/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.intuitivetools.primeirotestecomkotlindsl
 
-import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,7 +22,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +31,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            PrimeiroTesteComKotlinDSLTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
         }
     }
 }
@@ -46,22 +51,55 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleLarge
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
-            Color(0xFFD0BCFF)
+            Surface(shape = MaterialTheme.shapes.medium, tonalElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+fun PreviewConversation() {
+    PrimeiroTesteComKotlinDSLTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
+
+/*
+@Preview(name = "Light Mode")
+@Preview(uiMode= Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
+@Composable
+fun PreviewMessageCard() {
+    PrimeiroTesteComKotlinDSLTheme {
+        Surface {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
+    }
+}
+*/
 
