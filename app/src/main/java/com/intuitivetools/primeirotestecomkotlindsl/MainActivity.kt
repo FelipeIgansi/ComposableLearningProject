ac25b3efd05698ad81bba576550905efbc55d2d3
FelipeIgansi <lipeigansi@gmail.com>
2023-08-07 14:48:03 -0300

    Finish of project default compose

--- app/src/main/java/com/intuitivetools/primeirotestecomkotlindsl/MainActivity.kt
+++ app/src/main/java/com/intuitivetools/primeirotestecomkotlindsl/MainActivity.kt
@@ -3,8 +3,11 @@ package com.intuitivetools.primeirotestecomkotlindsl
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,6 +23,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -33,7 +40,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrimeiroTesteComKotlinDSLTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -54,19 +61,35 @@ fun MessageCard(msg: Message) {
                 .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
 
         )
+
+        var isExpanded by remember { mutableStateOf(false) }
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            label = "Show the full body message"
+        )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleLarge
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, tonalElevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                tonalElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.bodySmall
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
                 )
             }
         }
@@ -89,17 +112,3 @@ fun PreviewConversation() {
         Conversation(SampleData.conversationSample)
     }
 }
-
-/*
-@Preview(name = "Light Mode")
-@Preview(uiMode= Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
-@Composable
-fun PreviewMessageCard() {
-    PrimeiroTesteComKotlinDSLTheme {
-        Surface {
-            MessageCard(Message("Android", "Jetpack Compose"))
-        }
-    }
-}
-*/
-
